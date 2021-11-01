f41c9ee2fbe1d4b8bfa864f7c1bfc5e87a3b180e
wsxfvhu <krmiv77@gmail.com>
2021-11-01 10:43:41 +0300

    итог

--- Zadacha1/Program.cs
+++ Zadacha1/Program.cs
@@ -10,6 +10,27 @@ namespace Zadacha1
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите слова через пробел: ");
+            string input = Console.ReadLine();
+            string[] str = input.Split();
+            int maxLen = 0, index = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i].Length > maxLen)
+                {
+                    maxLen = str[i].Length;
+                    index = i;
+                }
+            }
+            Console.Write("Самое длинное слово: {0}", str[index]);
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i].Length == maxLen && i != index)
+                {
+                    Console.Write(" и {0}", str[i]);
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
--- Zadacha2/Program.cs
+++ Zadacha2/Program.cs
@@ -10,6 +10,25 @@ namespace Zadacha2
     {
         static void Main(string[] args)
         {
+            string startString = Console.ReadLine();
+            string[] stringArray = startString.Split();
+            string resultString = "";
+            int f = 0;
+            foreach (string s in stringArray)
+            {
+                resultString += s.Substring(0, 1).ToLower() + s.Substring(1);
+            }
+            for (int i = 0, j = resultString.Length - 1; i < resultString.Length; i++, j--)
+            {
+                if (resultString[i] != resultString[j] && i != j)
+                {
+                    f = 1;
+                }
+            }
+            if (f == 0)
+                Console.WriteLine("{0},{1}", startString, "Полиндром");
+            else Console.WriteLine("{0},{1}", startString, "Не полиндром");
+            Console.ReadLine();
         }
     }
 }
